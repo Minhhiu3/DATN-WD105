fa4cc322d47df322020059837da0fe4ec6ef4598
Nguyen Ba The Vy <bavy3663@gmail.com>
2025-05-23 09:43:43 +0700

    home-shop-product-detail

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('client.pages.home');
+})->name('home');
+Route::get('/products', function () {
+    return view('client.pages.products');
+})->name('products');
+Route::get('/product-detail', function () {
+    return view('client.pages.product-detail');
+})->name('product-detail');
